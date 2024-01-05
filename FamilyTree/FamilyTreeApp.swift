95d4d0f6e7515b05e43958b5223df887cda64bcd
Addison Mirliani <addisonbm@icloud.com>
2024-01-05 08:28:35 -0500

    first commit

---
+++ FamilyTree/FamilyTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyTreeApp.swift
+//  FamilyTree
+//
+//  Created by Addison Mirliani on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FamilyTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
